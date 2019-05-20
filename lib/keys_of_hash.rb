0dfb936c3ee4aa95a0533d48029397ded2b2b201
Kolton Starr <starrkolton@gmail.com>
2019-05-20 18:52:37 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,12 @@
 class Hash
   def keys_of(*arguments)
-    
-    animals
-    arr = [] 
+    arr = []
+    animals.each do |key, value|
+      if value == arguments
+      arr << key 
+      end
+    end
+    arr
   end
+  
 end
